eb4e23441706104eaa1abc2ae259ecff4481e610
Gwyn Morfey <gwyn@Unknown-00-23-6c-89-83-be.config>
2010-05-07 23:34:24 +0100

    Refactored. Pretty but doesn't work.

--- src/com/gwyn/sshtest/TestActivity.java
+++ src/com/gwyn/sshtest/TestActivity.java
@@ -14,55 +14,68 @@ import java.io.InputStreamReader;
 
 public class TestActivity extends Activity {
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+	static final String SSH_HOST = "192.168.1.67";
+	static final String SSH_USER = "Gwyn Morfey";
+	static final String SSH_PASS = "kitt*()";
+	Session mysession;
 
-		
-
-		
-		JSch jsch=new JSch();  
-		try {
-			Session session = jsch.getSession("Gwyn Morfey","192.168.1.67",22);
-			
+	public Session setupSession() throws JSchException {
+			JSch jsch=new JSch();  
+			Session session = jsch.getSession(SSH_USER,SSH_HOST,22);
 			java.util.Properties config = new java.util.Properties(); 
 			config.put("StrictHostKeyChecking", "no");
 			session.setConfig(config);
+			session.setPassword(SSH_PASS);
 
-			session.setPassword("kitt*()");
 			session.connect();
 
-			String command = "ls -l /";
+		return session;
+	}
+	public String runCommand(Session session, String cmd) {
+		try {
 			ChannelExec channel = (ChannelExec) session.openChannel("exec");
-			channel.setCommand(command);
+			channel.setCommand(cmd);
 			channel.connect();
-			Log.v("taggy","***********************" + "***CONNECTED");
-
 
 			BufferedReader reader;
 			reader = new BufferedReader(new InputStreamReader(channel.getInputStream()));
 
 			StringBuilder responseBuilder = new StringBuilder();
-			SystemClock.sleep(1000);
+			Log.v("taggy","start sleep");
+			SystemClock.sleep(200); //command must begin sending output within 1000ms!
+			Log.v("taggy","end sleep..");
+
 			while (!channel.isClosed() && !channel.isEOF()) {
+				Log.v("taggy","reading a line");
 				String line = reader.readLine();
 				if (line != null) {
 					responseBuilder.append(line).append("\n");
 				}
 			}
-			Log.v("taggy","********** HERE: " + responseBuilder.toString() + " **************");
-			Log.v("taggy","**************************DONE");
+			Log.v("taggy","returning");
 
+			return responseBuilder.toString();
 		} catch (JSchException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();	
+			return "KABOOM";
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "KABOOM";
 		}
+	}
 
-
-
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		try {
+			mysession = setupSession();
+			Log.v("taggy","***!!!!!!!!!!**********" + runCommand(mysession,"ls ~"));
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
